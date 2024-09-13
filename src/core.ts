7d198e9ac5e185524e250130a47fff541591802a
darylsong <darylsong@tencent.com>
2024-09-13 18:00:42 +0800

    first commit

---
+++ src/core.ts
@@ -0,0 +1,42 @@
+import { ReactNode, createContext, createElement, useContext, useState, FC, useEffect } from 'react';
+
+interface Container {
+  useContainer: () => any;
+  Provider: (props: { children: ReactNode }) => JSX.Element;
+}
+const handleContentVal = () => {
+  const listeners: any = {};
+  return {
+    delete: (key: string) => delete listeners[key],
+    get: (key: string) => (key ? listeners[key] : listeners),
+    set: (key: string, val: any) => (listeners[key] = val),
+  };
+};
+const globalSelfState = handleContentVal();
+
+const conts: Container[] = [];
+
+// 状态管理
+const createContainer = (defaultValue: any, defName = '') => {
+  const Context = createContext<any>({});
+  const temp: Container = {
+    useContainer: () => useContext(Context),
+    Provider: ({ children }: { children: ReactNode }) => {
+      const value = useState(defaultValue);
+      const [state] = value;
+      useEffect(() => {
+        globalSelfState.set(defName, state);
+      }, [state]);
+      return createElement(Context.Provider, { value }, children);
+    },
+  };
+  conts.push(temp);
+  return temp;
+};
+
+const ComposeProvider: FC<any> = ({ children }) => {
+  return conts.reduceRight((child, { Provider }) => createElement(Provider, null, child), children);
+};
+
+
+export default { ComposeProvider, createContainer, globalSelfState }
